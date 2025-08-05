8b54550209d3bf20012346e98e8fe958183d851b
Deepak Dewatwal <136556140+Dewatwal@users.noreply.github.com>
2025-08-05 12:51:25 +0530

    modified

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+import mongoose, { mongo } from "mongoose";
+
+// Function to connect to the mongodb database
+const connectDB = async ()=>{
+  mongoose.connection.on('connected',()=>console.log('DB connected'))
+  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+}
+export default  connectDB
